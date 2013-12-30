6217334deccd40f58e22d3e663bac966c39b540c
S <s>
2013-12-30 18:52:15 +0800

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -129,6 +129,7 @@ public class Breakout extends GraphicsProgram {
 	private void drawPaddle() {
 		// locate the initial location of paddle
 		double paddle_x = ;
+		double paddle_y = HEIGHT - PADDLE_Y_OFFSET;
 		
 		// draw the paddle
 		paddle = new GRect (paddle_x, paddle_y, PADDLE_WIDTH, PADDLE_HEIGHT);
