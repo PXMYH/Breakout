8a33936b956e44d0263b4a821f7ba27263e2f4e5
S <s>
2013-12-30 20:31:49 +0800

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -158,6 +158,8 @@ public class Breakout extends GraphicsProgram {
 	
 	private void drawBall() {
 		double ball_x, ball_y;
+		ball_x = (WIDTH - BALL_RADIUS) / 2;
+		ball_y = ;
 		ball = new GOval();
 	}
 }
