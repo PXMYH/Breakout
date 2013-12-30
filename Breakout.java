b3544f118616270ae3fc46c3fba6e44fbb80e95a
S <s>
2013-12-30 19:47:36 +0800

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -141,7 +141,7 @@ public class Breakout extends GraphicsProgram {
 		if (e.getX() < WIDTH - PADDLE_WIDTH) {
 			drawPaddle(e.getX(), HEIGHT - PADDLE_Y_OFFSET);
 		} else 
-			drawPaddle(e.getX());
+			drawPaddle(, HEIGHT - PADDLE_Y_OFFSET);
 	}
 	
 	
