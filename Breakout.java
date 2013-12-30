9ff91902d0bb5f36f0ae0d93a83ce25a5c135b95
S <s>
2013-12-30 20:21:05 +0800

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -60,7 +60,7 @@ public class Breakout extends GraphicsProgram {
 
 /* Method: run() */
 /** Runs the Breakout program. */
-	public void run() {
+	public void init() {
 		// draw the game initial setup
 		drawGameBricks();
 		
