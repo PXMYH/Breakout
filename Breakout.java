6acc5ba242d0f315b287e665b64cd72f4c438ffb
S <s>
2013-12-30 14:55:49 +0800

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -84,7 +84,17 @@ public class Breakout extends GraphicsProgram {
 			
 			// draw the bricks
 			switch (index) {
-			case 0: brick_color = Color.red;
+			case 0: brick_color = Color.red;break;
+			case 1: brick_color = Color.red;break;
+			case 2: brick_color = Color.red;break;
+			case 3: brick_color = Color.red;break;
+			case 4: brick_color = Color.red;break;
+			case 5: brick_color = Color.red;break;
+			case 6: brick_color = Color.red;break;
+			case 7: brick_color = Color.red;break;
+			case 8: brick_color = Color.red;break;
+			case 9: brick_color = Color.red;break;
+			case 10: brick_color = Color.red;break;
 			}
 			drawBrick(brick_x, brick_y, brick_color);
 		}
