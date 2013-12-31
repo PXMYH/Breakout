f2529fd24273e61215f7f71e020752d1f2239db8
S <s>
2013-12-31 11:59:38 +0800

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -59,7 +59,7 @@ public class Breakout extends GraphicsProgram {
 	private GRect paddle;
 	private GOval ball;
 	
-	private static final int GAME_DELAY = 30;
+	private static final int GAME_DELAY = 15;
 
 	private double Vx, Vy;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
