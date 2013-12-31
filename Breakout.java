52cdc9ba4396d1eb0d0391c2c01874bf53ba09eb
S <s>
2013-12-31 18:23:22 +0800

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -253,6 +253,6 @@ public class Breakout extends GraphicsProgram {
 	}
 	
 	private void terminateGame(){
-		terminate = new GLabel ();
+		terminate = new GLabel ("@_@ GAME OVER @_@", WIDTH / 2, HEIGHT / 2);
 	}
 }
