e67f3fbaafe676cd1a7fe27152dcd52342fa4a1f
S <s>
2013-12-31 14:10:02 +0800

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -67,19 +67,19 @@ public class Breakout extends GraphicsProgram {
 	
 	/* Method: init() */
 	/* Initialize the program */
-	public void init() {
-		// draw the game initial setup
-		drawGameBricks();
-		
-		// draw the paddle
-		// locate the initial location of paddle
-		double paddle_x = (WIDTH - PADDLE_WIDTH)/ 2;
-		double paddle_y = HEIGHT - PADDLE_Y_OFFSET;		
-		drawPaddle(paddle_x, paddle_y);
-		
-		// add listener for mouse movement
-		addMouseListeners();
-	}
+//	public void init() {
+//		// draw the game initial setup
+//		drawGameBricks();
+//		
+//		// draw the paddle
+//		// locate the initial location of paddle
+//		double paddle_x = (WIDTH - PADDLE_WIDTH)/ 2;
+//		double paddle_y = HEIGHT - PADDLE_Y_OFFSET;		
+//		drawPaddle(paddle_x, paddle_y);
+//		
+//		// add listener for mouse movement
+//		addMouseListeners();
+//	}
 	
 	/* Method: run() */
 	/** Runs the Breakout program. */
