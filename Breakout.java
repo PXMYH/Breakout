4dacb48fd569f56239f7a4ad13696569bf25c942
S <s>
2013-12-31 12:42:06 +0800

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -228,7 +228,7 @@ public class Breakout extends GraphicsProgram {
 						return null;
 		
 			else 
-				return getElementAt(ball.getX() + BALL_RADIUS, ball.getY() + BALL_RADIUS * 2;
+				return getElementAt(ball.getX() + BALL_RADIUS, ball.getY() + BALL_RADIUS * 2);
 		else 
 			return getElementAt(ball.getX(), ball.getY());
 	}
